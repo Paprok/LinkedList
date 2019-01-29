af953d26361351114baaab2755e5b3e1d7c8c7f3
Paprok <rafal.becla@gmail.com>
2019-01-29 17:59:08 +0100

    created tests for add method

--- src/test/java/com/codecool/krk/LinkedListTest.java
+++ src/test/java/com/codecool/krk/LinkedListTest.java
@@ -6,12 +6,18 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class LinkedListTest {
 
+    private void fillListWithNumbers(LinkedList<Integer> list, int last) {
+        for (int i = 0; i <= last; i++) {
+            list.append(i);
+        }
+    }
+
     @Test
     void testAppendOneItem() {
-        String expected = "1";
         LinkedList<Integer> list = new LinkedList<>();
-
         list.append(1);
+
+        String expected = "1";
         String actual = list.toString();
 
         assertEquals(expected, actual);
@@ -19,21 +25,82 @@ public class LinkedListTest {
 
     @Test
     void testAppendMultipleItems() {
-        String expected = "0 1 2 3 4 5 100 200";
         LinkedList<Integer> list = new LinkedList<>();
-
-        int i = 5;
-        fillListWithNumbers(list, i);
+        fillListWithNumbers(list, 5);
         list.append(100);
         list.append(200);
+
+        String expected = "0 1 2 3 4 5 100 200";
         String actual = list.toString();
 
         assertEquals(expected, actual);
     }
 
-    private void fillListWithNumbers(LinkedList<Integer> list, int last) {
-        for (int i = 0; i <= last; i++) {
-            list.append(i);
-        }
+    @Test
+    void testAddTo0IndexEmptyList() {
+        LinkedList<Integer> list = new LinkedList<>();
+        list.add(0, 666);
+
+        String expected = "666";
+        String actual = list.toString();
+
+        assertEquals(expected, actual);
     }
+
+    @Test
+    void testAddTo0IndexFilledList() {
+        LinkedList<Integer> list = new LinkedList<>();
+        fillListWithNumbers(list, 6);
+        list.add(0, 666);
+
+        String expected = "666 0 1 2 3 4 5 6";
+        String actual = list.toString();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testAddToNegativeIndexThrowsException(){
+        LinkedList<Integer> list = new LinkedList<>();
+
+        assertThrows(IndexOutOfBoundsException.class,
+
+                ()-> list.add(-1, 1));
+    }
+
+    @Test
+    void testAddToIndexHigherThanSizeThrowsException(){
+        LinkedList<Integer> list = new LinkedList<>();
+        list.append(1);
+
+        assertThrows(IndexOutOfBoundsException.class,
+
+                ()-> list.add(99, 1));
+    }
+
+    @Test
+    void testAddToMidIndex(){
+        LinkedList<Integer> list = new LinkedList<>();
+        fillListWithNumbers(list, 6);
+        list.add(3, 666);
+
+        String expected = "0 1 2 666 3 4 5 6";
+        String actual = list.toString();
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testAddToLastIndex(){
+        LinkedList<Integer> list = new LinkedList<>();
+        fillListWithNumbers(list, 6);
+        list.add(7, 666);
+
+        String expected = "0 1 2 3 4 5 6 666";
+        String actual = list.toString();
+
+        assertEquals(expected, actual);
+    }
+
+    
 }
