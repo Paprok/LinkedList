652b7475a207741a8c11befb6813587de348a0a1
Paprok <rafal.becla@gmail.com>
2019-01-29 19:43:46 +0100

    passed tests for remove method

--- src/main/java/com/codecool/krk/linkedList/LinkedList.java
+++ src/main/java/com/codecool/krk/linkedList/LinkedList.java
@@ -1,4 +1,4 @@
-package com.codecool.krk;
+package com.codecool.krk.linkedList;
 
 public class LinkedList<T> {
     private LinkedNode<T> first;
--- src/main/java/com/codecool/krk/linkedList/LinkedNode.java
+++ src/main/java/com/codecool/krk/linkedList/LinkedNode.java
@@ -1,4 +1,4 @@
-package com.codecool.krk;
+package com.codecool.krk.linkedList;
 
 public class LinkedNode<T> {
 //    private int index;
